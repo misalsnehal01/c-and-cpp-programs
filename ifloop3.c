960511aed48817282dfa8adf82d5451485953218
misalsnehal01 <misalsnehal80@gmail.com>
2025-07-05 09:55:07 +0530

    Add files via upload

---
+++ ifloop3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main () {
+	printf("start main\n");
+	int x=0;
+	int y=10;
+	if(x) {
+		printf("in first If -block \n");
+	}
+	if(y) {
+		printf("in second If -block\n");
+	}
+	printf("End main \n");
+}
