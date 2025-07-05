2a5af32f4717168f6892c9a7a222294bb6e9e5f1
misalsnehal01 <misalsnehal80@gmail.com>
2025-07-05 10:06:15 +0530

    Add files via upload

---
+++ ifloop10.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main() {
+printf("Start Main \n");
+char ch1 ='A';
+char ch2 ='48';
+if (ch1) {
+	printf("In first if block\n");
+}
+printf("ch2 = %c \n",ch2);
+if (ch2) {
+	printf("In second if block \n");
+}
+printf("End main\n");
+}
