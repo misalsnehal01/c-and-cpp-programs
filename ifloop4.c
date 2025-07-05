d839a645e14e683eac05d9cc86d7983c9da61ecf
misalsnehal01 <misalsnehal80@gmail.com>
2025-07-05 09:55:37 +0530

    Add files via upload

---
+++ ifloop4.c
@@ -0,0 +1,16 @@
+//try and error
+#include<stdio.h>
+void main () {
+	printf("start main\n");
+	char ch1='A';
+	//char ch2='48';
+	char ch2=48;
+	if(ch1) {
+		printf("In first If - block\n");
+	}
+	if(ch2) {
+		printf("In second If - block \n");
+	}
+	printf("end main\n");
+}
+
