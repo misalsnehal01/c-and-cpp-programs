e99510f7efe251bc548843aae68d47d8c0828d15
misalsnehal01 <misalsnehal80@gmail.com>
2025-07-05 09:53:29 +0530

    Add files via upload

---
+++ elseif3.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void main () {
+	int x;
+	printf("Enter value of x b/w 1-11\n");
+	scanf("%d",&x);
+	if(x==1) {
+		printf("one\n");
+		} else if(x==2) {
+                printf("Two\n");
+		} else if(x==3) {
+                printf("Three\n");
+		} else if(x==4) {
+                printf("Four\n");
+		} else if(x==5) {
+                printf("Five\n");
+		} else if(x==6) {
+                printf("Six\n");
+		} else if(x==7) {
+                printf("Seven\n");
+		} else if(x==8) {
+                printf("Eight\n");
+		} else if(x==9) {
+                printf("Nine\n");
+		} else if(x==10) {
+                printf("Ten\n"); 
+		} else if(x==11) {
+                printf("Eleven\n");
+		}
+	        else {
+			printf("please Enter the value in b/w 1-11\n");
+		}
+}
