319b3b579935340d4459b105bc46faa04562ac2d
misalsnehal01 <misalsnehal80@gmail.com>
2025-07-05 10:08:45 +0530

    Add files via upload

---
+++ logicalop2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main () {
+	int x=8;
+	int y=11;
+	int ans;
+	ans = ++x || ++y;
+	printf("%d\n",x);
+        printf("%d\n",ans);
+        printf("%d\n",y);
+	ans = x++ && y++;
+        printf("%d\n",x);
+        printf("%d\n",ans);
+        printf("%d\n",y);
+}
